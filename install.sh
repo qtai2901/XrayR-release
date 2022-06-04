b9df3e767eeafd2d2d5a4f052f02e3cf478c9083
qtai2901 <67884469+qtai2901@users.noreply.github.com>
2022-06-05 02:01:33 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -77,9 +77,12 @@ install_base() {
     if [[ x"${release}" == x"centos" ]]; then
         yum install epel-release -y
         yum install wget curl unzip tar crontabs socat -y
+	firewall-cmd --zone=public --add-port=80/tcp --permanent
+	firewall-cmd --reload
     else
         apt update -y
         apt install wget curl unzip tar cron socat -y
+	ufw allow 80
     fi
 }
 
